0bf19ce29785b3d6278812c83efbd5646a46464e
13pub <13public@gmail.com>
2022-05-24 12:18:52 +0300

    init

---
+++ src/main/kotlin/ru/protei/SpringBootKotlinEdu/SpringBootKotlinEduApplication.kt
@@ -0,0 +1,11 @@
+package ru.protei.SpringBootKotlinEdu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinEduApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootKotlinEduApplication>(*args)
+}
---
+++ src/test/kotlin/ru/protei/SpringBootKotlinEdu/SpringBootKotlinEduApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.protei.SpringBootKotlinEdu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinEduApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
